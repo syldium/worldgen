d10e3ccf6424f8782c2570ea18c22722db6f060b
syldium <syldium@mailo.com>
2020-08-02 13:23:28 +0200

    fix: ci

--- src/components/biome/BiomeStarts.jsx
+++ src/components/biome/BiomeStarts.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useEffect } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import Select from 'react-select';
 import { STARTS } from './BiomeDefaults';
 import { useJsonEffect } from '../../hooks/form';
