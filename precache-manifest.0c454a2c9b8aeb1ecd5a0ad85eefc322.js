692592839c279b509ef603bb55c365f5c2843615
syldium <syldium@mailo.com>
2020-08-07 15:54:53 +0000

    Deploying to gh-pages from  @ 25a3f6c0f4ffaa459b17e3048fe691ccae2d631c 🚀

---
+++ precache-manifest.0c454a2c9b8aeb1ecd5a0ad85eefc322.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9491a8610518c0fdc743c57e320c5f9f",
+    "url": "/worldgen/index.html"
+  },
+  {
+    "revision": "3bc5c8672ed75a0baab2",
+    "url": "/worldgen/static/css/main.178c51d3.chunk.css"
+  },
+  {
+    "revision": "a973f37008adc5a23517",
+    "url": "/worldgen/static/js/2.602b1a2c.chunk.js"
+  },
+  {
+    "revision": "e991a629d53db9276da8150ffc0bc99b",
+    "url": "/worldgen/static/js/2.602b1a2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bc5c8672ed75a0baab2",
+    "url": "/worldgen/static/js/main.c75cd5ef.chunk.js"
+  },
+  {
+    "revision": "c30c8b2e4aa14cdc7848",
+    "url": "/worldgen/static/js/runtime-main.f1f42b5a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/worldgen/precache-manifest.d2b116939e077dbcc0db4ef8fccb74cb.js"
+  "/worldgen/precache-manifest.0c454a2c9b8aeb1ecd5a0ad85eefc322.js"
 );
 
 self.addEventListener('message', (event) => {
