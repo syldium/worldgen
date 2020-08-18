c4e50537a13e08b992bec727f0b53631303e1e97
syldium <syldium@mailo.com>
2020-08-18 12:42:18 +0200

    fix: the carver `ocean_cave` doesn't exist

--- src/components/carver/CarverDefaults.js
+++ src/components/carver/CarverDefaults.js
@@ -2,7 +2,6 @@ export const VANILLA_CARVERS = [
     { value: 'canyon', label: 'Canyon', probability: 0.02 },
     { value: 'cave', label: 'Cave', probability: 0.143 },
     { value: 'nether_cave', label: 'Nether cave', probability: 0.2 },
-    { value: 'ocean_cave', label: 'Ocean cave', probability: 0.067 },
     { value: 'underwater_canyon', label: 'Underwater canyon', probability: 0.02  },
     { value: 'underwater_cave', label: 'Underwater cave', probability: 0.067 }
 ].map(option => {
