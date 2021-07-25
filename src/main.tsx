d5a7ed47d2bd01888d4831b3d3f14f617e142b87
syldium <syldium@mailo.com>
2021-07-25 16:12:42 +0200

    fix(style): re-add space helpers

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.css';
+import './style/spacers.scss';
 import { DatapackApp } from './main/component/DatapackApp';
 import { GameRegistryProvider } from './main/context/GameRegistry';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <GameRegistryProvider>
         <DatapackApp />
       </GameRegistryProvider>
