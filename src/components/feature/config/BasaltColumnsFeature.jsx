c77058b9cf48e285c1960c216c113c036b9c27ff
syldium <syldium@mailo.com>
2021-07-25 11:20:34 +0200

    Fix the basalt columns configuration (#10)

--- src/components/feature/config/BasaltColumnsFeature.jsx
+++ src/components/feature/config/BasaltColumnsFeature.jsx
@@ -5,17 +5,17 @@ import { UniformInt } from '../../utils/UniformInt';
 export function BasaltColumnsFeature({ configuration, onChange }) {
 
     const handleHeightChange = useCallback(function (height) {
-        onChange({ ...configuration, height });
+        onChange({ ...configuration, ...height });
     }, [configuration, onChange]);
     const handleReachChange = useCallback(function (reach) {
-        onChange({ ...configuration, reach });
+        onChange({ ...configuration, ...reach });
     }, [configuration, onChange]);
 
     return <fieldset>
         <legend>Config</legend>
         <div className="form-group form-row">
-            <UniformInt id="reach" value={configuration.reach} maxBase={2} maxSpread={2} onChange={handleReachChange}>Reach</UniformInt>
-            <UniformInt id="height" value={configuration.height} minBase={1} maxBase={5} maxSpread={5} onChange={handleHeightChange}>Height</UniformInt>
+            <UniformInt id="reach" value={configuration.reach} maxBase={2} maxSpread={2} upChange={handleReachChange}>Reach</UniformInt>
+            <UniformInt id="height" value={configuration.height} minBase={1} maxBase={5} maxSpread={5} upChange={handleHeightChange}>Height</UniformInt>
         </div>
     </fieldset>
 }
