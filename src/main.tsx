1b424340a78dc7555a6020ba521096123889adcd
syldium <syldium@mailo.com>
2022-03-29 19:58:03 +0200

    dep: update to React 18 and bump dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
 import { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
 import { DatapackApp } from './main/component/DatapackApp';
 import { GameRegistryProvider } from './main/context/GameRegistry';
 import './style/index.css';
 import './style/spacers.scss';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
   <StrictMode>
     <GameRegistryProvider>
       <DatapackApp location={window.location.pathname} />
     </GameRegistryProvider>
     <Toaster />
-  </StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
