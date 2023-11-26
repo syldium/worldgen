4642e8fa7c21dedb098da8f838dc0d39faf53c3a
syldium <syldium@mailo.com>
2023-11-26 18:46:24 +0100

    refactor: use browser style for input[type=input]

--- src/main/component/ui/NumberInput.tsx
+++ src/main/component/ui/NumberInput.tsx
@@ -1,14 +1,5 @@
-import {
-  ChangeEventHandler,
-  InputHTMLAttributes,
-  MouseEvent,
-  useEffect,
-  useRef
-} from 'react';
+import { ChangeEventHandler, InputHTMLAttributes, ReactElement } from 'react';
 import { INT_MAX_VALUE, INT_MIN_VALUE } from '../../util/MathHelper';
-
-const STEP_INTERVAL = 150;
-const STEP_DELAY = 300;
 
 // @ts-ignore
 interface NumberInputProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -26,117 +17,23 @@ export function NumberInput({
   size,
   onChange,
   ...props
-}: NumberInputProps): JSX.Element {
+}: NumberInputProps): ReactElement {
   const inputSize = size ?? Math.max(String(value).length, 3) + 1;
-
-  const ref = useRef<HTMLInputElement>(null);
   const handleChange: ChangeEventHandler<HTMLInputElement> = function (event) {
     const input = event.target;
-    const number = input.valueAsNumber;
-    if (isNaN(number)) {
-      input.setCustomValidity('Not a number');
-    } else {
-      input.setCustomValidity('');
-      onChange(number);
-    }
+    onChange(input.valueAsNumber);
   };
-  const handleStep = function (up: boolean) {
-    let number = value + (up ? step : -step);
-    if (step < 1) {
-      number = parseFloat(number.toPrecision(5));
-    }
-    if (number > max) {
-      if (!up) {
-        number = max;
-      } else {
-        return;
-      }
-    } else if (number < min) {
-      if (up) {
-        number = min;
-      } else {
-        return;
-      }
-    }
-    ref.current!.setCustomValidity('');
-    value = number;
-    onChange(value);
-  };
-
-  const timeoutRef = useRef<number>();
-  const handleMouseDown = function (event: MouseEvent) {
-    event.preventDefault();
-
-    const up = (event.target as HTMLButtonElement).classList.contains(
-      'btn-plus'
-    );
-    handleStep(up);
-    timeoutRef.current = window.setTimeout(() => {
-      function loopStep() {
-        handleStep(up);
-        timeoutRef.current = window.setTimeout(loopStep, STEP_INTERVAL);
-      }
-
-      timeoutRef.current = window.setTimeout(loopStep, STEP_DELAY);
-    }, STEP_DELAY);
-  };
-
-  const onStopStep = () => {
-    clearTimeout(timeoutRef.current);
-  };
-
-  useEffect(() => onStopStep, []);
-
   const stepValue = step < 1 ? 'any' : step;
-
-  if (import.meta.env.SSR) {
-    return (
-      <input
-        type="number"
-        min={min}
-        max={max}
-        step={stepValue}
-        size={inputSize}
-        value={value}
-        onChange={handleChange}
-        {...props}
-      />
-    );
-  }
-
   return (
-    <div className="number-input">
-      <div className="number-wrapper">
-        <input
-          type="number"
-          min={min}
-          max={max}
-          step={stepValue}
-          size={inputSize}
-          value={value}
-          onChange={handleChange}
-          ref={ref}
-          {...props}
-        />
-        <div className="number-controls">
-          <button
-            className="btn-plus"
-            onMouseUp={onStopStep}
-            onMouseLeave={onStopStep}
-            onMouseDown={handleMouseDown}
-            onClick={(e) => e.preventDefault()}
-            aria-label="Increase value"
-          />
-          <button
-            className="btn-minus"
-            onMouseUp={onStopStep}
-            onMouseLeave={onStopStep}
-            onMouseDown={handleMouseDown}
-            onClick={(e) => e.preventDefault()}
-            aria-label="Decrease value"
-          />
-        </div>
-      </div>
-    </div>
+    <input
+      type="number"
+      min={min}
+      max={max}
+      step={stepValue}
+      size={inputSize}
+      value={value}
+      onChange={handleChange}
+      {...props}
+    />
   );
 }
